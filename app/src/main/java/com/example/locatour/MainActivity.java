ece50f9c9c813e4a32b676d68085413a84c7ef0d
HitlersFavoriteJew <129925554+Luwijiiiiiiii@users.noreply.github.com>
2025-10-21 20:33:05 +0800

    intent for profile

--- app/src/main/java/com/example/locatour/MainActivity.java
+++ app/src/main/java/com/example/locatour/MainActivity.java
@@ -47,12 +47,14 @@ public class MainActivity extends AppCompatActivity {
         });
         PersonBtn  = findViewById(R.id.PersonBtn);
         PersonBtn.setOnClickListener( new View.OnClickListener(){
+
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
                 StyleableToast.makeText(MainActivity.this, "Welcome to Profile!", R.style.comingSoon).show();
             }
-            Intent intent = new Intent(this, ProfileActivity::class.java)
+
         });
     }
 }
